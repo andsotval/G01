de0a3d4aed4a473586a151aca6cf471d8690b763
Andres <andressotovaldes@gmail.com>
2017-01-13 12:01:32 +0100

    Initial proyect

---
+++ G01/src/division/Test.java
@@ -0,0 +1,10 @@
+package division;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Division.division(8, 2));
+	}
+
+}
---
+++ G01/src/multiplicacion/Multiplicacion.java
@@ -0,0 +1,9 @@
+package multiplicacion;
+
+public class Test {
+
+	public static int multiplicacion(int a, int b){
+		return a*b;
+	}
+
+}
